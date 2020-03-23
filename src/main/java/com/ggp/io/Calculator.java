a73a72e4dbc48f94548635f7050b21b672e13272
gongguanpeng@xdja.com <gongguanpeng@xdja.com>
2020-03-23 17:17:04 +0800

    bio demo

---
+++ src/main/java/com/ggp/io/Calculator.java
@@ -0,0 +1,16 @@
+package com.ggp.io;
+
+/**
+ * @author: ggp
+ * @Date: 2020/3/23 14:19
+ * @Description:
+ */
+public class Calculator {
+    public static int addCal(String expression){
+        int index = expression.indexOf("+");
+        int a = Integer.parseInt(expression.substring(0,index));
+        int b = Integer.parseInt(expression.substring(index+1));
+        return a+b;
+    }
+
+}
---
+++ src/main/java/com/ggp/io/bio/ServerHandler.java
@@ -0,0 +1,57 @@
+package com.ggp.io.bio;
+
+import com.ggp.io.Calculator;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * @author: ggp
+ * @Date: 2020/3/23 14:09
+ * @Description:
+ */
+public class ServerHandler implements Runnable{
+    private Socket socket;
+
+    public ServerHandler(Socket socket){
+        this.socket = socket;
+    }
+
+    public void run() {
+        BufferedReader in = null;
+        PrintWriter out = null;
+
+        try {
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(),true);
+            /**
+             * 接收客户端发过来的消息
+             */
+            String expression;
+            /**
+             * 计算结果返回给客户端
+             */
+            String result;
+            while (true){
+                if(null == (expression = in.readLine())){
+                    break;
+                }else{
+                    System.out.println("服务器收到消息："+expression);
+                }
+                result = String.valueOf(Calculator.addCal(expression));
+                out.println(result);
+            }
+        }catch (Exception e){
+            if(null != in){
+                try {
+                    in.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+    }
+}
