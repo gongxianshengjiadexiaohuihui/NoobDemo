76dd1531701de80c7e48b6318787443f77a13399
ggp <937827303@qq.com>
2020-12-26 18:32:50 +0800

    一致性hash算法,没有虚拟节点--补充测试用例

--- algorithm-demo/src/test/java/com/ggp/noob/demo/algorithm/hash/consistent/ConsistentHashingWithoutVirtualNodeTest.java
+++ algorithm-demo/src/test/java/com/ggp/noob/demo/algorithm/hash/consistent/ConsistentHashingWithoutVirtualNodeTest.java
@@ -14,7 +14,27 @@ public class ConsistentHashingWithoutVirtualNodeTest {
         ConsistentHashingWithoutVirtualNode hashing = new ConsistentHashingWithoutVirtualNode();
         String[] servers = new String[]{"11.12.86.49:8080","11.12.86.50:8080","11.12.86.51:8080","11.12.86.52:8080","11.12.86.53:8080"};
         hashing.init(servers);
-        for (int i = 0; i <100 ; i++) {
+        for (int i = 0; i <20 ; i++) {
+            hashing.putServer("test"+i,i);
+        }
+        hashing.print();
+    }
+    @Test
+    public void test_add_server(){
+        ConsistentHashingWithoutVirtualNode hashing = new ConsistentHashingWithoutVirtualNode();
+        String[] servers = new String[]{"11.12.86.49:8080","11.12.86.50:8080","11.12.86.51:8080","11.12.86.52:8080","11.12.86.53:8080","11.12.86.54:8080"};
+        hashing.init(servers);
+        for (int i = 0; i <20 ; i++) {
+            hashing.putServer("test"+i,i);
+        }
+        hashing.print();
+    }
+    @Test
+    public void test_del_server(){
+        ConsistentHashingWithoutVirtualNode hashing = new ConsistentHashingWithoutVirtualNode();
+        String[] servers = new String[]{"11.12.86.49:8080","11.12.86.50:8080","11.12.86.52:8080","11.12.86.53:8080"};
+        hashing.init(servers);
+        for (int i = 0; i <20 ; i++) {
             hashing.putServer("test"+i,i);
         }
         hashing.print();
